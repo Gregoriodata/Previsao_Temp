9e4dc6adc93610154f8c24276306db033759367c
Marcos Gregorio Farias Lima <gregoriodata@gmail.com>
2022-10-17 14:56:50 -0300

    Try_except

--- Previ_Temp.py
+++ Previ_Temp.py
@@ -4,27 +4,30 @@ import json
 import pyodbc
 from datetime import date, datetime
 
+dados_conexao = (
+    "Driver={SQL Server};"  # Drive do banco utilizado.
+    "Server=DESKTOP-NQ82MK2;"  # CMD HOSTNAME
+    "Database=PRE_TEMPO;")  # Nome do Banco de dados
+conexao = pyodbc.connect(dados_conexao)
+print("Conexao com o banco realizada com sucesso")
+
 a = ('Curitiba', 'Brasilia', 'Maranhão')
 for icity in a:
     data = datetime.today().replace(microsecond=0)
-    #hour = time
     idi = 'pt_br'
     itoken = '95bad896508cf4e07d03dcbe6b0ee65c'
     icity = icity.upper()
-    iurl = f'https://api.openweathermap.org/data/2.5/weather?q={icity}&appid={itoken}&lang={idi}'
-    requisicao = requests.request("GET", iurl)
-    iretorno_req = json.loads(requisicao.text)
-    print(iretorno_req['cod'])
-    descri = iretorno_req['weather'][0]['description']
-    temp = round(iretorno_req['main']['temp'] - 273.15, 1)
+    try:
+        iurl = f'https://api.openweathermap.org/data/2.5/weather?q={icity}&appid={itoken}&lang={idi}'
+        requisicao = requests.request("GET", iurl)
+        iretorno_req = json.loads(requisicao.text)
+        print(iretorno_req['cod'])
+        descri = iretorno_req['weather'][0]['description']
+        temp = round(iretorno_req['main']['temp'] - 273.15, 1)
+    except ConnectionError as error:
+        print(error)
     print(
         f'A Cidade de {icity},no data {data}, apresenta céu {descri}, com temperatura em {temp}')
-    dados_conexao = (
-        "Driver={SQL Server};"  # Drive do banco utilizado.
-        "Server=DESKTOP-NQ82MK2;"  # CMD HOSTNAME
-        "Database=PRE_TEMPO;")  # Nome do Banco de dados
-    conexao = pyodbc.connect(dados_conexao)
-    print("Conexao com o banco realizada com sucesso")
     cursor = conexao.cursor()
     comando = f"""INSERT INTO PREV_TEMP(CIDADE,DATA,TEMP,DESCRI)
     VALUES('{icity}','{data}','{temp}','{descri}')"""
