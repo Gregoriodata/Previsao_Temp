126aa615afb2f29044540f17e7737965e3814a8f
Marcos Gregorio Farias Lima <gregoriodata@gmail.com>
2022-10-18 15:04:51 -0300

    Exit_Print_ajuste_Var

--- Previ_Temp.py
+++ Previ_Temp.py
@@ -10,27 +10,28 @@ dados_conexao = (
     "Database=PRE_TEMPO;")  # Nome do Banco de dados
 conexao = pyodbc.connect(dados_conexao)
 print("Conexao com o banco realizada com sucesso")
+data = datetime.today().replace(microsecond=0)
+idi = 'pt_br'
+itoken = '95bad896508cf4e07d03dcbe6b0ee65c'
+
 
 a = ('Curitiba', 'Brasilia', 'Maranhão')
 for icity in a:
-    data = datetime.today().replace(microsecond=0)
-    idi = 'pt_br'
-    itoken = '95bad896508cf4e07d03dcbe6b0ee65c'
     icity = icity.upper()
     try:
         iurl = f'https://api.openweathermap.org/data/2.5/weather?q={icity}&appid={itoken}&lang={idi}'
         requisicao = requests.request("GET", iurl)
         iretorno_req = json.loads(requisicao.text)
         print(iretorno_req['cod'])
-        descri = iretorno_req['weather'][0]['description']
-        temp = round(iretorno_req['main']['temp'] - 273.15, 1)
-    except ConnectionError as error:
-        print(error)
+        clima = iretorno_req['weather'][0]['description']
+        temperatura = round(iretorno_req['main']['temp'] - 273.15, 1)
+    except:
+        exit(print("Erro de Conexao, API não está respondendo, o programa será finalizado"))
     print(
-        f'A Cidade de {icity},no data {data}, apresenta céu {descri}, com temperatura em {temp}')
+        f'A Cidade de {icity},no data {data}, apresenta céu {clima}, com temperatura em {temperatura}')
     cursor = conexao.cursor()
-    comando = f"""INSERT INTO PREV_TEMP(CIDADE,DATA,TEMP,DESCRI)
-    VALUES('{icity}','{data}','{temp}','{descri}')"""
+    comando = f"""INSERT INTO PREVI_TEMPO(CIDADE,DATA,TEMPERATURA,CLIMA)
+    VALUES('{icity}','{data}','{temperatura}','{clima}')"""
     # print(comando)
     cursor.execute(comando)
     cursor.commit()
